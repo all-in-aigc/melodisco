51a6cec8cd2e79e0a6c73ee166968367eec9f43e
idoubi <me@idoubi.cc>
2024-04-06 17:57:48 +0800

    add ga

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -47,6 +47,33 @@ export default function RootLayout({
         <link rel="alternate" hrefLang="fr" href="https://melodis.co/fr/" />
         <link rel="alternate" hrefLang="zh-CN" href="https://melodis.co/zh/" />
         <link rel="alternate" hrefLang="x-default" href="https://melodis.co" />
+
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-TYVCKPTYH1"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+            
+              gtag('config', 'G-TYVCKPTYH1');
+            `,
+          }}
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','G-TYVCKPTYH1');
+            `,
+          }}
+        ></script>
       </head>
       <body className={inter.className}>
         <NextIntlClientProvider locale={locale} messages={messages}>
